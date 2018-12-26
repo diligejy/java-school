000da9a00de7bc74346727354c01833e64fbad72
diligejy <40735154+diligejy@users.noreply.github.com>
2018-12-26 14:18:36 +0900

    Add files via upload

---
+++ src/com/test/FileTest08.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.io.*;
+
+public class FileTest08 {
+	public static void main(String[] args) throws IOException {
+		// Stream (byte) read() write() = String X
+		// char Read() = String X2
+		// 대상을 파일에서 읽어서 파일로 쓰곘다
+		File f = new File("parkboyoung.jpg");
+		FileInputStream fi = new FileInputStream(f);
+		BufferedInputStream bi = new BufferedInputStream(fi);
+		
+		File res = new File("pby.jpg");
+		FileOutputStream fo = new FileOutputStream(res);
+		BufferedOutputStream bfo = new BufferedOutputStream(fo);
+		int ch = 0;
+		
+		while((ch = bi.read()) != -1) {
+			bfo.write(ch);//BufferedInputStream(byte단위)2- 이미지, 음성, 동영상  etc Streaming 가능 한 것들 vs BufferedReader(char단위)- 문자 관리
+			
+		}
+		bfo.close();
+		fo.close();
+		bi.close();
+		fi.close();
+	}
+}
---
+++ src/com/test02/FileTest.java
@@ -0,0 +1,10 @@
+package com.test02;
+
+import java.io.*;
+
+public class FileTest {
+	public static void main(String[] args) {
+		//char를 읽고 쓰자. 단, 버퍼링을 하자.
+
+	}
+}
