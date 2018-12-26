5b6397dcf5d3ec07d9c69bfcd7b51903c22c523a
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 11:15:57 +0900

    file개수와 dir개수 측정

--- src/com/test/FileTest04.java
+++ src/com/test/FileTest04.java
@@ -19,14 +19,21 @@ public class FileTest04 {
 	public static void Prn02(File fi) {
 		//File[] listfiles() - Nonstatic 모두 가능
 		System.out.println("   listFiles()   ");
+		int file_count = 0;
+		int dir_count = 0;
+		
 		File[] res = fi.listFiles();
 		for (File m_list : res) {
 			if(m_list.isFile()) {
 				System.out.println("Filename : " + m_list);
+				file_count++;
 			}
 			if(m_list.isDirectory()) {
 				System.out.println("DirectoryName : " + m_list);
+				dir_count++;
 			}
 		}
+		System.out.println(" File의 개수 : " + file_count);
+		System.out.println(" Dir의 개수 : " + dir_count);
 	}
 }
