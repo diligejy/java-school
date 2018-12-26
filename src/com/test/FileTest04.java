438d339b64b207e5f4150e5aaedc83ccdaac5973
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 11:07:19 +0900

    list() - 이름만 나옴 listFiles() - 경로까지 나옴

---
+++ src/com/test/FileTest04.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.io.*;
+
+public class FileTest04 {
+	public static void main(String[] args) {
+		File fi = new File("C:\\Windows\\System32");
+		Prn(fi);
+		Prn02(fi);
+
+	}
+	public static void Prn(File fi) {
+		//public String[] list()
+		System.out.println(" list () ");
+		for(String m_list : fi.list()) {
+			System.out.println(m_list);
+		}
+	}
+	public static void Prn02(File fi) {
+		//File[] listfiles() - Nonstatic 모두 가능
+		System.out.println("   listFiles()   ");
+		File[] res = fi.listFiles();
+		for (File m_list : res) {
+			System.out.println(m_list);
+		}
+	}
+}
