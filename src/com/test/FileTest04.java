f2b29d7625d5ee9f44bf9685b289da0456edea95
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 11:13:23 +0900

    if문을 통해 isFile()과 isDirectory()실습

--- src/com/test/FileTest04.java
+++ src/com/test/FileTest04.java
@@ -21,8 +21,12 @@ public class FileTest04 {
 		System.out.println("   listFiles()   ");
 		File[] res = fi.listFiles();
 		for (File m_list : res) {
-			System.out.println(m_list.getPath() + " : " + m_list.getAbsolutePath());
-			System.out.println(m_list.getName() + " : " + m_list.getParent());
+			if(m_list.isFile()) {
+				System.out.println("Filename : " + m_list);
+			}
+			if(m_list.isDirectory()) {
+				System.out.println("DirectoryName : " + m_list);
+			}
 		}
 	}
 }
