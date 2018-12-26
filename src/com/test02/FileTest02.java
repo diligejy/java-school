ec4496119c05658d42588de7cfcd3fc09eaeecde
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 14:43:30 +0900

    FileOutputStream의 원형이라고 합네다!

--- src/com/test02/FileTest02.java
+++ src/com/test02/FileTest02.java
@@ -17,19 +17,22 @@ public class FileTest02 {
 
 	public static void My_Write(File fi) throws IOException {
 		FileOutputStream fos = null;
-		try (fos = new FileOutputStream(fi)) {
+		try {
+			fos = new FileOutputStream(fi);
 			for (int i = 65; i <= 90; i++) {
 				fos.write(i); // throws IOException
-
 			}
-		}catch(FileNotFoundException f) {
-			
-		}catch(IOException i) {
-			
-		}finally {
+
+		} catch (
+
+		FileNotFoundException f) {
+
+		} catch (IOException i) {
+
+		} finally {
 			try {
 				fos.close();
-			}catch (IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
