d6c140f9940082f9d202c4ec8ba44220a24d73ed
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 16:26:34 +0900

    직렬화 안에서만 쓸 수 있는 keyword로 입력값을 보내지 않는 것.

--- src/com/test02/Address.java
+++ src/com/test02/Address.java
@@ -1,5 +1,26 @@
 package com.test02;
 
-public class Address {
+import java.io.Serializable;
+
+public class Address implements Serializable {
+	private String name;
+	private transient String addr;
+	private String tel;
+
+	public Address() {
+		super();
+	}
+
+	public Address(String name, String addr, String tel) {
+		super();
+		this.name = name;
+		this.addr = addr;
+		this.tel = tel;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [name=" + name + ", addr=" + addr + ", tel=" + tel + "]";
+	}
 
 }
