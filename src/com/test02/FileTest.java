5c14e6a522f91a20aeddfce576ab294c42187077
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 14:20:16 +0900

    지금

--- src/com/test02/FileTest.java
+++ src/com/test02/FileTest.java
@@ -3,8 +3,11 @@ package com.test02;
 import java.io.*;
 
 public class FileTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//char를 읽고 쓰자. 단, 버퍼링을 하자.
-
+		//File, FileReader, BufferReader => ReadLine();
+		FileReader fr = new FileReader("b.txt");
+		
+		
 	}
 }
