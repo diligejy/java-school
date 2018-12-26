a3fcfdc5c956c2a8bf9f56015cd2002d3fd5b0f1
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 15:10:35 +0900

    try catch를 통해 close없이도 하게 됨

--- src/com/test/FileTest06.java
+++ src/com/test/FileTest06.java
@@ -4,18 +4,23 @@ import java.io.*;
 
 public class FileTest06 {
 	public static void main(String[] args) throws IOException {
+
+		// 파일 입출력
+		File fi = new File("a.txt"); // 현재 디렉토리(File(String Pathname)생성자)
+		try (FileWriter fw = new FileWriter(fi)) {
+			fw.write("abcdefg \n");
+			fw.write("abcdefg \n");
+			fw.write("abcdefg \n")
+		} catch (Exception e) {
+			
+		}
+
+		try(FileReader fr = new FileReader(fi)){ // return을 int로 하기 때문에 -1과 비교하기 위해서 ch를 int형으로 선언
+			int ch;
+			while ((ch = fr.read()) != -1) {
+				System.out.println((char) ch); // int형이므로 char로 형변환
+			}
 		
-		//파일 입출력 
-		File fi = new File("a.txt"); //현재 디렉토리(File(String Pathname)생성자)
-		FileWriter fw = new FileWriter(fi);
-		fw.write("abcdefg \n");
-		fw.write("abcdefg \n");
-		fw.close();
-		
-		FileReader fr = new FileReader(fi); //return을 int로 하기 때문에 -1과 비교하기 위해서 ch를 int형으로 선언
-		int ch; 
-		while( (ch = fr.read()) != -1){
-			System.out.println((char)ch); //int형이므로 char로 형변환
 		}
 	}
 }
