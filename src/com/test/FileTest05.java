5e312139258abb4a522ae4620ac207cf5e8a58be
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 11:41:33 +0900

    디렉토리 안에 디렉토리가 아닌 파일이 있는 경우 isNullException이 발생할 수 있음

--- src/com/test/FileTest05.java
+++ src/com/test/FileTest05.java
@@ -7,11 +7,11 @@ public class FileTest05 {
 		File fi = new File("C:\\MyTest");
 		View(fi);
 	}
-	public static int cnt = 0;
+
 	public static void View(File fi) {
-		
+		int cnt = 0;
 		System.out.println(cnt++ + "<========> " + fi.getName());
-		if (fi.exists()) {
+		if (fi.exists() && !fi.equals(null)) {//디렉토리 안에 디렉토리가 아닌 파일이 있는 경우 대비
 			for (File str : fi.listFiles()) {
 				View(str);
 				System.out.println(str);
