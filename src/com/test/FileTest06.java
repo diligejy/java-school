c37d71ec86cd12828ffa2c756326df3bf9d182e3
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 11:58:13 +0900

    FileReader 추가 char로 캐스터링

--- src/com/test/FileTest06.java
+++ src/com/test/FileTest06.java
@@ -8,7 +8,14 @@ public class FileTest06 {
 		//파일 입출력 
 		File fi = new File("a.txt"); //현재 디렉토리(File(String Pathname)생성자)
 		FileWriter fw = new FileWriter(fi);
-		fw.write("abcdefg");
+		fw.write("abcdefg \n");
+		fw.write("abcdefg \n");
 		fw.close();
+		
+		FileReader fr = new FileReader(fi);
+		int ch;
+		while( (ch = fr.read()) != -1){
+			System.out.println((char)ch);
+		}
 	}
 }
