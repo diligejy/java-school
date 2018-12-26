b4c1d31a275c865cef20df5930dc0b7d045a29ac
Playdata <Playdata@DESKTOP-4U2BN04>
2018-12-26 12:35:05 +0900

    My_Read()메소드 추가 - 한글자씩 읽어서 리턴

--- src/com/test/FileTest07.java
+++ src/com/test/FileTest07.java
@@ -16,15 +16,25 @@ public class FileTest07 {
 	}
 
 	public static void My_Write(File fi) throws IOException {
-		FileOutputStream fos = new FileOutputStream(fi, true); //throws FileOutputStream
-		for(int i = 65; i <= 90; i++) {
+		FileOutputStream fos = new FileOutputStream(fi, true); // throws FileOutputStream
+		for (int i = 65; i <= 90; i++) {
 			fos.write(i); // throws IOException
-			
+
 		}
 		fos.close();
 	}
 
-	public static void My_Read(File fi) {
+	public static void My_Read(File fi) throws IOException {
+		FileInputStream fr = new FileInputStream(fi);
+		// 한글자씩 읽어서 리턴하자 글자 없으면 -1을 리턴한다
 
+		int ch = 0;
+
+		// 한글자씩 -1이 아닐때까지 읽어서 리턴하자
+
+		while ((ch = fr.read()) != -1) {
+			System.out.println((char) ch);
+		}
+		fr.close();
 	}
 }
